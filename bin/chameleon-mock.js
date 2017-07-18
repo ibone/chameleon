c2bf4279a82a448748c273b779dec75f1503c684
ibone <water2212683@gmail.com>
2017-07-18 19:01:57 +0800

    CHG use commander.js

--- bin/chameleon-mock.js
+++ bin/chameleon-mock.js
@@ -1,103 +1,69 @@
-#!/usr/bin/env node
-var chalk = require('chalk')
-var gutil = require('gulp-util')
-var Liftoff = require('liftoff')
-var v8flags = require('v8flags')
-var semver = require('semver')
-var tildify = require('tildify')
-var argv = require('minimist')(process.argv.slice(2))
+var program = require('commander')
+var fs = require('fs')
+var path = require('path')
 
-// Set env var for ORIGINAL cwd
-// before anything touches it
-process.env.INIT_CWD = process.cwd()
+program
+  .version('0.0.10')
 
-var cli = new Liftoff({
-  name: 'getman',
-  // completions: completion,
-  moduleName: 'getman',
-  extensions: { '.json': null },
-  v8flags: v8flags
-})
+program
+  .command('start')
+  .option('-p, --port <port>', 'set server port')
+  .action(function (env, options) {
+    if (!fs.existsSync('.chameleonrc')) {
+      console.log('no found config file \'.chameleonrc\' ')
+      console.log('use \'./node_modules/.bin/chameleon-mock init\' ')
+    }
 
-// Exit with 0 or 1
-var failed = false
-process.once('exit', function (code) {
-  if (code === 0 && failed) {
-    process.exit(1)
-  }
-})
+    var config = require('./.chameleonrc')
+    var ChameleonMock = require(env.modulePath)
+    config.appPath = path.join(process.cwd())
+    config.userSettingPath = path.join(config.appPath, 'chameleonsetting.json')
+    config.rootPath = path.join(config.appPath, config.rootPath)
+    config.mockPath = path.join(config.appPath, config.mockPath)
 
-cli.on('require', function (name) {
-  gutil.log('Requiring external module', chalk.magenta(name))
-})
+    ChameleonMock(config)
+    console.log('start chameleon-mock server http://localhost:8080')
+    console.log('visit chameleon-mock admin http://localhost:8080/chameleon-mock/admin')
+  })
 
-cli.on('requireFail', function (name) {
-  gutil.log(chalk.red('Failed to load external module'), chalk.magenta(name))
-})
+program
+  .command('init')
+  .action(function () {
+    console.log('init')
+    var configPath = path.join(process.cwd(), '.chameleonrc')
+    if (fs.existsSync(configPath)) {
+      console.log('chameleon-mock config file exist')
+    } else {
+      var config = {
+        'name': 'app name',
+        'port': '8080',
+        'mockPath': './mock',
+        'rootPath': './dist'
+      }
+      fs.writeFile('.chameleonrc', JSON.stringify(config), function (err) {
+        if (err) throw err
+        console.log('create chameleon-mock config file: .chameleonrc')
+      })
+    }
+  })
 
-cli.on('respawn', function (flags, child) {
-  var nodeFlags = chalk.magenta(flags.join(', '))
-  var pid = chalk.magenta(child.pid)
-  gutil.log('Node flags detected:', nodeFlags)
-  gutil.log('Respawned to PID:', pid)
-})
+program
+  .command('demo')
+  .action(function () {
+    console.log('start chameleon-mock server http://localhost:8080')
+    console.log('visit chameleon-mock admin http://localhost:8080/chameleon-mock/admin')
+    var configPath = path.join(__dirname, '../example/.chameleonrc')
+    var modulePath = path.join(__dirname, '../')
+    var text = fs.readFileSync(configPath, 'utf8')
+    var config = JSON.parse(text)
+    var ChameleonMock = require(modulePath)
 
-cli.launch({
-  cwd: argv.cwd,
-  configPath: argv.gulpfile,
-  require: argv.require,
-  completion: argv.completion
-}, handleArguments)
+    config.appPath = path.join(__dirname, '../example/')
+    config.userSettingPath = path.join(config.appPath, 'chameleonsetting.json')
+    config.rootPath = path.join(config.appPath, config.rootPath)
+    config.mockPath = path.join(config.appPath, config.mockPath)
 
-/*
-var port = argv.p;
-if(typeof port != 'number'){
-    gutil.log(chalk.red('chameleon need a server port'));
-    gutil.log('you can input:    $ chameleon -p 3000');
-    process.exit(1);
-}
-*/
+    ChameleonMock(config)
+  })
 
-var cliPackage = require('../package')
-
-function handleArguments (env) {
-  if (!env.modulePath) {
-    gutil.log(
-      chalk.red('Local chameleon not found in'),
-      chalk.magenta(tildify(env.cwd))
-    )
-    gutil.log(chalk.red('Try running: npm install mock'))
-    process.exit(1)
-  }
-
-  if (!env.configPath) {
-    gutil.log(chalk.red('No .chameleonrc found'))
-    process.exit(1)
-  }
-
-  // Check for semver difference between cli and local installation
-  if (semver.gt(cliPackage.version, env.modulePackage.version)) {
-    gutil.log(chalk.red('Warning: chameleon version mismatch:'))
-    gutil.log(chalk.red('Global chameleon is', cliPackage.version))
-    gutil.log(chalk.red('Local chameleon is', env.modulePackage.version))
-  }
-
-  // Chdir before requiring gulpfile to make sure
-  // we let them chdir as needed
-  if (process.cwd() !== env.cwd) {
-    process.chdir(env.cwd)
-    gutil.log(
-      'Working directory changed to',
-      chalk.magenta(tildify(env.cwd))
-    )
-  }
-
-  var config = require(env.configPath)
-  config.cwd = env.cwd
-  if (!config.rootPath) {
-    config.rootPath = env.cwd
-  }
-  gutil.log('Using chameleonfile', chalk.magenta(tildify(env.configPath)))
-  var mock = require(env.modulePath)
-  mock(config)
-}
+program.parse(process.argv)
