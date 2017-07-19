a21e5baf54066d2cded93c19c932c425bcfe4c59
ibone <water2212683@gmail.com>
2017-07-19 18:21:12 +0800

    CHG custom plugin optimize

--- plugins/custom.js
+++ plugins/custom.js
@@ -1,6 +1,23 @@
-function main (option, req, mockData, callback) {
-  mockData = mockData.replace('{{chameleon.uuid(8)}}', Math.floor(Math.random() * 100000000))
-  callback(mockData)
+var ejs = require('ejs')
+var _ = require('lodash')
+var globalFn = {
+  number: function (len) {
+    if (!_.isNumber(len)) {
+      len = 10
+    }
+    if (len === 1) {
+      return _.random(1, 9)
+    }
+    return _.random(Math.pow(10, len - 1), Math.pow(10, len) - 1)
+  },
+  id: function () {
+    return _.uniqueId('id_')
+  }
+}
+
+function main (option, mockData, env, callback) {
+  var result = ejs.render(mockData, globalFn)
+  callback(result)
 }
 
 module.exports = main
